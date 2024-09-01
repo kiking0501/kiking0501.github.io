f8c4df410fd95fc853b651047ddd0fceaa38bd83
kiking0501 <kiking0501@gmail.com>
2024-09-01 04:23:22 -0400

    Add Graph View (level-1 bookmarks) for reading notes

---
+++ reading_notes/main.js
@@ -0,0 +1,111 @@
+        
+$( document ).ready(function() {
+    display_book_covers();
+    display_graph();
+    $("#nav_pills_display").find("li").on('click', function(){change_nav_pill(this)});
+    $("#display_graph").click();
+
+    var init_pos = order[0];
+    load_book(init_pos);
+})
+
+
+function change_nav_pill(obj) {
+    $("#nav_pills_display").find("li").removeClass("active");
+    $(obj).addClass("active");
+
+    $(".display_tab").hide();
+    if ($(obj).attr('id') == "display_standard") {$("#section_book_cover").show();}
+    if ($(obj).attr('id') == "display_graph") {$("#section_graph").show();}
+}
+
+import * as THREE from '//unpkg.com/three/build/three.module.js';
+import SpriteText from "//unpkg.com/three-spritetext/dist/three-spritetext.mjs";
+
+function display_graph() {
+
+    function getImgSprite(book_key) {
+        var title = reading_info[book_key]['title'];
+        var imgElement = $("#cover_" + title).find("img")[0];
+
+        const imgTexture = new THREE.TextureLoader().load(imgElement.src);
+        imgTexture.colorSpace = THREE.SRGBColorSpace;
+        const material = new THREE.SpriteMaterial({ map: imgTexture });
+        const sprite = new THREE.Sprite(material);
+
+        var discountFactor = 2.0
+        sprite.scale.set(
+            imgElement.width / discountFactor,
+            imgElement.height * (imgElement.width / imgElement.naturalWidth) / discountFactor
+        );
+        return sprite;
+    }
+
+    function getTextSprite(title, color) {
+        const sprite = new SpriteText(title);
+        sprite.material.depthWrite = false; // make sprite background transparent
+        sprite.color = color;
+        sprite.textHeight = 6;
+        return sprite;
+    }
+
+
+    const Graph = ForceGraph3D()(document.getElementById('section_graph'))
+      .width(750)
+      .height(1200)
+      .backgroundColor('black')
+      .showNavInfo(false)
+      .jsonUrl('./graph/complete_graph.json')
+      .dagMode('zin')
+      .zoomToFit(10, 20, node => true)
+      .linkColor((link) => {
+        if (link.type == "contains") { return reading_info[link.source]['color'] };
+        if (link.type == "related") { return 'red'}
+        if (link.type == "read_sequence") { return 'lightgrey' }
+      })
+      .linkOpacity(.5)
+      .linkWidth((link)=> {
+        if (link.type == "contains") { return 0 };
+        if (link.type == "related") { return 2 };
+        if (link.type == "read_sequence") { return 0 }        
+      })
+      .nodeThreeObject((node) => {
+        if (node.type == "img") {
+            return getImgSprite(node.book_key);
+        }
+        if (node.type == "text") {
+            return getTextSprite(node.title, reading_info[node.book_key]['color']);
+        }
+      })
+      .onNodeClick((node) => {
+        if (node.type == "text") {
+            load_book(node.book_key, node.bookmark_page)
+        } else {
+            load_book(node.book_key);
+        }
+      });
+      Graph.d3Force('charge').strength(-150);
+}
+
+
+function display_book_covers() {
+    var section_book_covers = $("#section_book_cover")
+    section_book_covers.html("");
+    for (let i = 0; i < order.length; i++) {
+        var book_key = order[i];
+        var book_cover = $("#template_book_cover").clone();
+
+        var title = reading_info[book_key]['title'];
+        var glink = reading_info[book_key]['glink'];
+        book_cover.find(".book_cover").attr("id", "cover_" + title);
+        
+        var img = book_cover.find("img")[0];
+        $(img).attr("id", "img_" + title);
+        $(img).attr("src","./media/covers/cover_" + title + ".jpg");
+        $(img).attr("title", $("#description_" + title).text());
+        $(img).attr("onclick", "load_book(" + book_key + ")");
+        section_book_covers.append(book_cover.html());
+    }
+}
+
+
